06eb21a6c650eca82f1e33731b18a50324b2f2d3
Michal Chvíla <electrydev@gmail.com>
2016-09-05 13:19:58 +0200

    msm: memory.h: Add hardboot page mem address for K920
    Change-Id: I8f03ca64b96b837198094596a649d861aa9bc0fb
    Signed-off-by: Michal Chvíla <electrydev@gmail.com>

--- arch/arm/mach-msm/include/mach/memory.h
+++ arch/arm/mach-msm/include/mach/memory.h
@@ -28,6 +28,10 @@
 #elif defined(CONFIG_MACH_MSM8974_HAMMERHEAD)
 #define KEXEC_HB_PAGE_ADDR		UL(0x10100000)
 #define KEXEC_HB_KERNEL_LOC		UL(0x3208000)
+#elif defined(CONFIG_MACH_LENOVO_K920)
+/* memblock_end_of_DRAM() - PERSISTENT_RAM_SIZE - SZ_1M */
+#define KEXEC_HB_PAGE_ADDR		UL(0xDFE00000)
+#define KEXEC_HB_KERNEL_LOC		UL(0x3208000)
 #else
 #error "Adress for kexec hardboot page not defined"
 #endif
